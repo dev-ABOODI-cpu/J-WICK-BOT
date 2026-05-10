de502f171afaa0eb4d8da65c72f765b11656b39e
dev-ABOODI-cpu <rm9051010@gmail.com>
2026-05-11 02:52:19 +0300

    Update example1.js

--- plugins/example/example1.js
+++ plugins/example/example1.js
@@ -1,5 +1,5 @@
 /* 
-by: VA ~ VENOM
+by: VA ~ ABOODI 
 */
 
 const example = async (m, { conn }) => {
@@ -7,13 +7,13 @@ const example = async (m, { conn }) => {
 conn.msgUrl(m.chat,
   '*🔥 Special Offer*',
   {
-    img: 'https://i.postimg.cc/RV5Ypq7S/2d38929b4e6d0fe34b002805729dd9ba.jpg',
+    img: 'https://i.postimg.cc/50RZLD2X/21317022ddf5862432a39318f314e445.jpg',
     title: '50% OFF',
     body: 'Limited time',
     big: true,
     mentions: ['249113388050@s.whatsapp.net', '249112727808@s.whatsapp.net'],
     newsletter: {
-      name: '𝗝 𝗪𝗶𝗰𝗸 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+      name: '𝐑𝐔𝐒𝐓𝐀𝐌 ☣ ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥',
       jid: '120363425546384005@newsletter'
     }
   },
